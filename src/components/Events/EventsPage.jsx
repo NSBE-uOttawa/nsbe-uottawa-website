735cdee721271529ffdcf255c434956224957d6a
Osa <85007593+osaaa@users.noreply.github.com>
2023-12-25 13:41:03 -0500

    feat: events page

--- src/components/Events/EventsPage.jsx
+++ src/components/Events/EventsPage.jsx
@@ -1,10 +1,11 @@
+import EventsHomepage from "./EventsHomePage";
 const EventsPage = () => {
 	return (
 		<div className="flex flex-col items-center ">
 			<h3 className="text-center text-5xl font-extrabold  bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-indigo-900">
 				Events
 			</h3>
-			<p>Check back later for events...</p>
+			<EventsHomepage />
 		</div>
 	);
 };
